98a1a60fc0a08979b01df53df4e68e78aa1816dd
Eldridge Turambi <eturambi95@gmail.com>
2022-10-13 13:50:35 -0600

    update migrations again

--- db/migrate/20221013194046_create_patients.rb
+++ db/migrate/20221013194046_create_patients.rb
@@ -2,7 +2,10 @@ class CreatePatients < ActiveRecord::Migration[5.2]
   def change
     create_table :patients do |t|
       t.string :name
-      t.array :symptoms
+      t.string :illness
+      t.references :doctor, foreign_key: true
+
+      t.timestamps
     end
   end
 end
